c7923a4d864ea8d357032d4f454bc4f8455c25b9
Pablo Saraiva <pablosaraiva@gmail.com>
2015-06-24 18:36:55 -0300

    Basic Level

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Destroy (gameObject);
+	}
+
+}
---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+		public GameObject playerCamera;
+
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+
+			Vector3 cameraPosition = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, playerCamera.transform.position.z);
+
+			playerCamera.transform.position = cameraPosition;
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
