ce5e136cdc6abaa49c5e66e8f26d7b78e0d66ef5
Renato Lazzarini <whosrenato@hotmail.com>
2015-07-17 14:34:13 -0300

    Created PickUpItems Files, Sprites and Script
    https://trello.com/c/FLjK7oUS/80-mecanica-conceito-objetivos-capturaveis

---
+++ Assets/PickUpItems/Scripts/CoinPickUp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickUp : MonoBehaviour {
+
+	public int pointToAdd;
+
+	void OnTriggerEnter2D (Collider other) {
+
+		//if (other.GetComponent<PlatformerCharacter2D> () == null)
+			//return;
+
+		ScoreManager.AddPoints (pointToAdd);
+
+		Destroy (gameObject);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
