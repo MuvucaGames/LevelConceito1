3c91f7f3d9ed88739bcc2fadf52ed159f5ade987
Rafael Vargas <rsvargas@gmail.com>
2015-07-24 02:45:24 -0300

    conceito de escolha de personagens

---
+++ Assets/PlayerSelect/Scripts/ReSkinAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+public class ReSkinAnimation : MonoBehaviour {
+
+	public string spriteSheetName;
+
+	void LateUpdate () {
+
+		var subSprites = Resources.LoadAll<Sprite>("Characters/" + spriteSheetName);
+
+		foreach (var renderer in GetComponentsInChildren<SpriteRenderer>())
+		{
+			string spriteName = renderer.sprite.name;
+			var newSprite = Array.Find(subSprites, item => item.name == spriteName);
+
+			if (newSprite)
+				renderer.sprite = newSprite;
+		}
+	}
+}
